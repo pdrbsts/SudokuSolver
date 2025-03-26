395e6044e1a816fdae5ca83b025be2b1f41a2e43
pdrbsts <140499109+pdrbsts@users.noreply.github.com>
2025-03-26 11:36:17 +0000

    Add files via upload

--- Sudoku.py
+++ Sudoku.py
@@ -12,12 +12,12 @@ class SudokuSolver:
         self.entries = []
         vcmd = (root.register(self.validate_input), '%P', '%V', '%W')
         for i in range(9):
-            row = []
+            row_entries = []
             for j in range(9):
                 entry = tk.Entry(self.grid_frame, width=3, font=('Courier', 16),
                                justify='center', validate='key', validatecommand=vcmd,
                                relief=tk.SOLID, borderwidth=1)
-                
+
                 pad_left = 1
                 pad_right = 1
                 pad_top = 1
@@ -29,7 +29,7 @@ class SudokuSolver:
                 if i % 3 == 0:
                     pad_top = 3
                 if i == 8:
-                     pad_bottom = 3
+                    pad_bottom = 3
 
                 entry.grid(row=i, column=j, padx=(pad_left, 0), pady=(pad_top, 0), ipadx=3, ipady=3, sticky="nsew")
 
@@ -38,14 +38,14 @@ class SudokuSolver:
                 entry.bind("<Left>", self.handle_arrow_keys)
                 entry.bind("<Right>", self.handle_arrow_keys)
                 entry.bind("<Return>", self.handle_arrow_keys)
+                entry.bind("<BackSpace>", self.handle_backspace)
 
-                row.append(entry)
-            self.entries.append(row)
+                row_entries.append(entry)
+            self.entries.append(row_entries)
 
         for i in range(9):
             self.grid_frame.grid_rowconfigure(i, weight=1)
             self.grid_frame.grid_columnconfigure(i, weight=1)
-
 
         self.buttons_frame = tk.Frame(root)
         self.buttons_frame.pack(pady=10)
@@ -91,6 +91,39 @@ class SudokuSolver:
         else:
             pass
 
+    def handle_backspace(self, event):
+        widget = event.widget
+        current_value = widget.get()
+        cursor_pos = widget.index(tk.INSERT)
+
+        if not current_value or cursor_pos == 0:
+            try:
+                current_row = widget.grid_info()['row']
+                current_col = widget.grid_info()['column']
+            except KeyError:
+                return
+
+            prev_col = current_col - 1
+            prev_row = current_row
+
+            if prev_col < 0:
+                prev_col = 8
+                prev_row -= 1
+
+            if prev_row < 0:
+                return
+
+            prev_row = max(0, prev_row)
+
+            try:
+                prev_widget = self.entries[prev_row][prev_col]
+                prev_widget.focus_set()
+                prev_widget.select_range(0, tk.END)
+                return "break"
+            except IndexError:
+                 pass
+
+        return
 
     def validate_input(self, new_value, validation_type, widget_name):
         if validation_type == 'key':
@@ -103,12 +136,9 @@ class SudokuSolver:
                     try:
                         current_row = widget.grid_info()['row']
                         current_col = widget.grid_info()['column']
-
                         self.root.after_idle(lambda cr=current_row, cc=current_col: self.move_focus_after_input(cr, cc))
-
                     except (KeyError, IndexError):
                          pass
-
                     return True
                 return False
             except ValueError:
@@ -127,7 +157,6 @@ class SudokuSolver:
             except IndexError:
                 pass
 
-
     def solve(self):
         try:
             grid = []
@@ -140,7 +169,6 @@ class SudokuSolver:
 
             original_fixed = [[(grid[i][j] != 0) for j in range(9)] for i in range(9)]
 
-
             if self.solve_sudoku(grid):
                 for i in range(9):
                     for j in range(9):
@@ -150,7 +178,6 @@ class SudokuSolver:
 
                         self.entries[i][j].delete(0, tk.END)
                         self.entries[i][j].insert(0, str(grid[i][j]))
-
             else:
                 messagebox.showerror("Error", "No solution exists for this puzzle")
         except ValueError:
@@ -164,7 +191,6 @@ class SudokuSolver:
         if self.entries:
             self.entries[0][0].focus_set()
 
-
     def solve_sudoku(self, grid):
         empty = self.find_empty(grid)
         if not empty:
@@ -175,7 +201,7 @@ class SudokuSolver:
                 grid[row][col] = num
                 if self.solve_sudoku(grid):
                     return True
-                grid[row][col] = 0
+                grid[row][col] = 0 # Backtrack
         return False
 
     def find_empty(self, grid):
@@ -200,23 +226,19 @@ class SudokuSolver:
                     return False
         return True
 
+
 if __name__ == "__main__":
     root = tk.Tk()
     app = SudokuSolver(root)
 
     root.update_idletasks()
-
     window_width = root.winfo_width()
     window_height = root.winfo_height()
-
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
-
     center_x = (screen_width // 2) - (window_width // 2)
     center_y = (screen_height // 2) - (window_height // 2)
-
     root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
-
     root.resizable(False, False)
 
     root.mainloop()
